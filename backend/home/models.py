2e4080b06826647b7794c3b8cf497229b071abbd
Crowdbotics <team@crowdbotics.com>
2020-08-27 15:03:55 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,18 +7,18 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150,)
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
@@ -25,8 +26,18 @@ class HomePage(models.Model):
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
+
+
+class Sample(models.Model):
+    "Generated Model"
+    address1 = models.CharField(max_length=256,)
+    address2 = models.CharField(max_length=256,)
+    city = models.CharField(max_length=256,)
+    state = models.CharField(max_length=5,)
+    zip = models.IntegerField()
+    date = models.DateField()
